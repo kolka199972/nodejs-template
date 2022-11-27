0ed9cbd9090dbf6525eefc511a127695ffcec903
kolka72 <kolia199972@gmail.com>
2022-11-28 00:51:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const yargs = require('yargs')
+const {addNote, printNotes, removeNote} = require('./notes.controller')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note',
+  builder: {
+    title: {
+      type: 'string',
+      describe: 'Note title',
+      demandOption: true
+    }
+  },
+  handler: ({title}) => addNote(title)
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'Print all notes',
+  handler: () => printNotes()
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove note by id',
+  builder: {
+    id: {
+      type: 'string',
+      describe: 'Note id',
+      demandOption: true
+    }
+  },
+  handler: ({id}) => removeNote(id)
+})
+
+yargs.parse()
